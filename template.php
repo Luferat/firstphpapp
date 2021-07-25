4dc2840cc3974a473e18bcac06683b31c4427828
André Luferat <contato@luferat.net>
2021-07-25 11:27:01 -0300

    Update template.php

--- template.php
+++ template.php
@@ -1,3 +1,19 @@
+<?php
+
+/**
+ * template.php
+ * Página modelo para criação do tema do site.
+ * Esta página servirá de modelo para todas as outras quando o tema estiver pronto.
+ */
+
+// Define constante com o diretório raiz (/) do aplicativo
+// Também podemos usar a constante mágica __DIR__
+define('PATH', $_SERVER['DOCUMENT_ROOT']);
+
+// Importa arquivo de configuração da página
+require_once(PATH . '/config/config.php');
+
+?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 
@@ -17,14 +33,27 @@
 
     <div class="wrap">
 
-        <header></header>
+        <header>
+            <a href="/"><img src="/img/logo02.png" alt="Meu Primeiro Site"></a>
+            <h1>Meu Primeiro App<small>Primeiros passos no PHP</small></h1>
+        </header>
 
-        <nav></nav>
+        <nav>
+            <a href="/"><i class="fas fa-pen-nib fa-fw"></i><span>Artigos</span></a>
+            <a href="/contacts"><i class="fas fa-comments fa-fw"></i><span>Contatos</span></a>
+            <a href="/about"><i class="fas fa-info-circle fa-fw"></i><span>Sobre</span></a>
+        </nav>
 
         <main>
             <article></article>
             <aside></aside>
         </main>
+
+        <footer>
+            <a href="/" title="Página inicial"><i class="fas fa-home fa-fw"></i></a>
+            <div>&copy; Copyright 2021 Seu Nome</div>
+            <a href="#top" title="Topo da página"><i class="fas fa-arrow-alt-circle-up fa-fw"></i></a>
+        </footer>
 
     </div>
 
