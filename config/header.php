8ff4e7648679ba6b74996e83100233c5e904416e
André Luferat <contato@luferat.net>
2021-07-26 12:07:52 -0300

    Update header.php

--- config/header.php
+++ config/header.php
@@ -35,6 +35,7 @@ if (file_exists('index.js'))
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="/global.css">
+    <?= $page_css ?>
     <style>
         /* Define o background da página */
         body {
